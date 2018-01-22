44ea32734a15b97e1a4f116b53f92dce164297d0
Ludvig Falck <lfalck@gmail.com>
2018-01-22 16:48:32 +0100

    Added function to create a Azure Storage SAS token query string.

--- src/CreateAzureSASToken.js
+++ src/CreateAzureSASToken.js
@@ -1,5 +1,5 @@
 /**
- * Create a Azure Shared Access Signature (SAS) token valid for one minute
+ * Create a Shared Access Signature (SAS) token valid for one minute for use with the Azure Service Bus REST API
  * Based on https://docs.microsoft.com/en-us/rest/api/eventhub/generate-sas-token but modified to use crypto-js for compatibility with Postman
  * @param resourceUri - The full uri of the resource, e.g. https://<yournamespace>.servicebus.windows.net
  * @param sasKeyName - The name of the Shared Access Signature key
@@ -8,7 +8,7 @@
  * {@link https://github.com/lfalck/AzureRestApiPostmanCollections GitHub}
  * 
  */
-function createSharedAccessToken(resourceUri, sasKeyName, sasKey) {
+function createServiceBusSASToken(resourceUri, sasKeyName, sasKey) {
     if (!resourceUri || !sasKeyName || !sasKey) {
         throw "Missing required parameter";
     }
@@ -21,4 +21,57 @@ function createSharedAccessToken(resourceUri, sasKeyName, sasKey) {
     const hash = CryptoJS.HmacSHA256(signatureUTF8, sasKey).toString(CryptoJS.enc.Base64);
     return 'SharedAccessSignature sr=' + encoded + '&sig=' +
         encodeURIComponent(hash) + '&se=' + ttl + '&skn=' + sasKeyName;
+}
+
+/**
+ * Create a Shared Access Signature (SAS) token query string valid for one minute for use with Azure Storage REST API
+ * @param accountName - The storage account name
+ * @param sasKey - The Shared Access Signature key
+ * @returns- A Shared Access Signature token query string
+ * {@link https://github.com/lfalck/AzureRestApiPostmanCollections GitHub}
+ * 
+ */
+function createStorageSASTokenQueryString(accountName, sasKey) {
+    if (!accountName || !sasKey) {
+        throw "Missing required parameter";
+    }
+
+    const now = new Date();
+    const minuteInMilliSeconds = 60000;
+    const signedPermission = "rwdlacup"; //Read, Write, Delete, List, Add, Create, Update, Process
+    const signedServices = "bfqt"; // Blob, File, Queue, Table
+    const signedResourceTypes = "sco"; // Service, Container, Object
+    const signedStart = new Date(now.getTime() - minuteInMilliSeconds).toISOString().slice(0, 19) + "Z";
+    const signedExpiry = new Date(now.getTime() + minuteInMilliSeconds).toISOString().slice(0, 19) + "Z";
+    const signedIP = "";
+    const signedProtocol = "https";
+    const signedVersion = "2017-04-17";
+
+    const inputvalue =
+        `${accountName}\n` +
+        `${signedPermission}\n` +
+        `${signedServices}\n` +
+        `${signedResourceTypes}\n` +
+        `${signedStart}\n` +
+        `${signedExpiry}\n` +
+        `${signedIP}\n` +
+        `${signedProtocol}\n` +
+        `${signedVersion}\n`;
+
+    const key = CryptoJS.enc.Base64.parse(sasKey);
+    let signature = CryptoJS.HmacSHA256(inputvalue, key);
+    signature = signature.toString(CryptoJS.enc.Base64);
+    signature = encodeURIComponent(signature);
+
+    const uri =
+        /*?sv=*/`${signedVersion}&` +
+        `ss=${signedServices}&` +
+        `srt=${signedResourceTypes}&` +
+        `sp=${signedPermission}&` +
+        `se=${signedExpiry}&` +
+        `st=${signedStart}&` +
+        `spr=${signedProtocol}&` +
+        `sig=${signature}`;
+
+    return uri;
 }
