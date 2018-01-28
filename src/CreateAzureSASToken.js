563c244666892a51771a0c02ec830d6869a4ff43
Ludvig Falck <lfalck@gmail.com>
2018-01-28 14:05:52 +0100

    Formatting and comments.

--- src/CreateAzureSASToken.js
+++ src/CreateAzureSASToken.js
@@ -36,35 +36,37 @@ function createStorageSASTokenQueryString(accountName, sasKey) {
         throw "Missing required parameter";
     }
 
-    const now = new Date();
-    const minuteInMilliSeconds = 60000;
-    const signedPermission = "rwdlacup"; //Read, Write, Delete, List, Add, Create, Update, Process
+    //Persmissions: Read, Write, Delete, List, Add, Create, Update, Process
+    const signedPermission = "rwdlacup";
     const signedServices = "bfqt"; // Blob, File, Queue, Table
     const signedResourceTypes = "sco"; // Service, Container, Object
+
     //Dates need to be in yyyy-dd-MMTHH:mm:ssZ format
-    const signedStart = new Date(now.getTime() - minuteInMilliSeconds).toISOString().slice(0, 19) + "Z";
-    const signedExpiry = new Date(now.getTime() + minuteInMilliSeconds).toISOString().slice(0, 19) + "Z";
+    const now = new Date();
+    const minuteInMilliSeconds = 60000;
+    const signedStart = new Date(now.getTime() - minuteInMilliSeconds)
+        .toISOString()
+        .slice(0, 19) + "Z"; //Remove milliseconds
+    const signedExpiry = new Date(now.getTime() + minuteInMilliSeconds)
+        .toISOString()
+        .slice(0, 19) + "Z";
+
     const signedIP = "";
     const signedProtocol = "https";
     const signedVersion = "2017-04-17";
 
     const inputvalue =
-        `${accountName}\n` +
-        `${signedPermission}\n` +
-        `${signedServices}\n` +
-        `${signedResourceTypes}\n` +
-        `${signedStart}\n` +
-        `${signedExpiry}\n` +
-        `${signedIP}\n` +
-        `${signedProtocol}\n` +
-        `${signedVersion}\n`;
+        `${accountName}\n` + `${signedPermission}\n` + `${signedServices}\n` +
+        `${signedResourceTypes}\n` + `${signedStart}\n` + `${signedExpiry}\n` +
+        `${signedIP}\n` + `${signedProtocol}\n` + `${signedVersion}\n`;
 
     const key = CryptoJS.enc.Base64.parse(sasKey);
     let signature = CryptoJS.HmacSHA256(inputvalue, key);
     signature = signature.toString(CryptoJS.enc.Base64);
     signature = encodeURIComponent(signature);
 
-    //The full SAS token query string is added to the sv parameter to reduce the length of the request URI in Postman
+    // The full SAS token query string is added to the sv parameter to
+    // reduce the length of the request URI in Postman 
     const uri =
         /*?sv=*/`${signedVersion}&` +
         `ss=${signedServices}&` +
